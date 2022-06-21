afa78568ffaef1926d318a0ad5df5851af8432c0
thecutehacker <78939673+thecutehacker@users.noreply.github.com>
2022-06-22 09:53:02 +1000

    Update Sub concat().vb

--- Sub concat().vb
+++ Sub concat().vb
@@ -2,41 +2,47 @@
 'Using spaces to format leads to odd formatting as the sheet is zoomed in or out.
 
 
-
-
 Sub concat()
 Dim match As Integer
 Dim nomore As Boolean
 Dim length As Integer
+Dim space_length As Integer
 Dim min_length As Integer
 Dim x As Integer
 For Each cell In Selection
 
 nomore = False
 length = Len(cell.Formula)
+min_length = 2
 
-For min_length = 2 To length + min_length 'increment all possible length of spaces.  String needs to be at least a character, otherwise no need to run.
+For space_length = 1 To length 'increment all possible length of spaces.  String needs to be at least a character, otherwise no need to run.
 
-x = Len(cell.Formula) - min_length 'invert increment counting from longest string of spaces to shortest.
+x = Len(cell.Formula) + min_length - space_length 'invert increment counting from longest string of spaces to shortest.
 
 
 'Generally we only want to remove one block of spaces
 'Instead of checking for every length of spaces and calling replace each time, find the first match, which will be the biggest
+On Error GoTo catch
+match = InStr(1, cell.Formula, space(x))
+If nomore = True Then
+GoTo catch
+End If
 
-match = InStr(1, cell.Formula, Space(x))
 If nomore = False Then
-    If match > 0 Then
+
+If match > 0 Then
+
 
     nomore = True
     'Once match is true jump to replace and remove the space
-    cell.Formula = Replace(cell.Formula, Space(x), "" + Chr(10), 1, 1, vbTextCompare)
+    cell.Formula = Replace(cell.Formula, space(x), "" + Chr(10), 1, 1, vbTextCompare)
 
     End If
+    
 End If
 
-Next min_length
+Next space_length
+
+catch:
 Next cell
-
-End Sub
-
-
+nomore = False
